783b3411ebc0c2b94dd351e5985ea5cc06ea11ee
Dev-Joco <csr8367@hanmail.net>
2023-01-06 22:36:56 +0900

    feat(tasks): Concurrency

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -3,6 +3,19 @@ package tasks
 import contributors.*
 import kotlinx.coroutines.*
 
+// solve 5: Concurrency
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        async {
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    deferreds.awaitAll().flatten().aggregate()
 }
