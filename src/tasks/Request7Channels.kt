f6c2634a7af385b22c7b3ba94a7fd46177a72a63
Dev-Joco <csr8367@hanmail.net>
2023-01-06 22:45:50 +0900

    feat(tasks): Channels

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -5,12 +5,32 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
+// solve 8: Channels
 suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) {
-    coroutineScope {
-        TODO()
+) = coroutineScope {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val channel = Channel<List<User>>()
+    for (repo in repos) {
+        launch {
+            val users = service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+            channel.send(users)
+        }
+    }
+
+    var allUsers = emptyList<User>()
+    repeat(repos.size) { index ->
+        val users = channel.receive()
+        allUsers = (allUsers + users).aggregate()
+
+        updateResults(allUsers, index == repos.lastIndex)
     }
 }
