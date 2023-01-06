e8816b4072575959bae17d213201f7fe10533bfb
Dev-Joco <csr8367@hanmail.net>
2023-01-06 22:41:22 +0900

    feat(tasks): Showing progress

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -2,10 +2,28 @@ package tasks
 
 import contributors.*
 
+// solve 7: Showing progress
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val allUsers = mutableListOf<User>()
+
+    for ((index, repo) in repos.withIndex()) {
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers += users
+        allUsers.aggregate()
+
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
